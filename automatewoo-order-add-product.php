47af645f01ea4caecc221d05b7e29e4b2b31dd09
NickGreen <nick.green@automattic.com>
2021-10-13 14:59:17 -0700

    Update automatewoo-order-add-product.php

--- automatewoo-order-add-product.php
+++ automatewoo-order-add-product.php
@@ -7,8 +7,8 @@
   function register_action( $actions ) {
 
     include_once ( plugin_dir_path( __FILE__ ) . 'includes/AbstractEditItem.php' );
-    include_once ( plugin_dir_path( __FILE__ ) . '/includes/Order_Edit_Product_Abstract.php' );
-    include_once ( plugin_dir_path( __FILE__ ) . '/includes/Order_Add_Product.php' );
+    include_once ( plugin_dir_path( __FILE__ ) . 'includes/Order_Edit_Product_Abstract.php' );
+    include_once ( plugin_dir_path( __FILE__ ) . 'includes/Order_Add_Product.php' );
 
     $actions['order_add_product'] = 'AutomateWoo\Action_Order_Add_Product';
 
